42064384717997bc59adf4826232e69767f8035a
NoCodeWorker <nocodeworker@github.com>
2026-02-02 19:02:44 +0100

    style: stack contact buttons vertically to fix email text overflow

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -89,7 +89,7 @@ const Contact: React.FC = () => {
               </div>
             </motion.a>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="flex flex-col gap-4">
               {/* Secondary Phone */}
               <motion.a
                 href={`tel:${phones.secondary.clean}`}
